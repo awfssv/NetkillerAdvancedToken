1cfbf16fcc5697d16d794411928c7a6f9d819752
Neo Chan <netkiller@msn.com>
2018-05-10 10:55:38 +0800

    Update NetkillerAdvancedTokenAirDrop.sol

--- contracts/NetkillerAdvancedTokenAirDrop.sol
+++ contracts/NetkillerAdvancedTokenAirDrop.sol
@@ -1,27 +1,25 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.21;
 
 /******************************************/
-/*    Netkiller Airdrop ADVANCED TOKEN    */
+/*       Netkiller ADVANCED TOKEN         */
 /******************************************/
 /* Author netkiller <netkiller@msn.com>   */
 /* Home http://www.netkiller.cn           */
-/* Version 2018-03-11                     */
+/* Version 2018-05-09 - Add Global lock   */
 /******************************************/
 
-interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
-
-contract NetkillerAirDropToken {
+contract NetkillerAdvancedToken {
     address public owner;
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 2;
+    uint public decimals;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
-
+    
     // This creates an array with all balances
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
+    mapping (address => uint256) internal balances;
+    mapping (address => mapping (address => uint256)) internal allowed;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -35,30 +33,31 @@ contract NetkillerAirDropToken {
     /* This generates a public event on the blockchain that will notify clients */
     event FrozenFunds(address target, bool frozen);
 
-    bool lock = true;                   // 锁仓
-    bool lockAirdrop = true;            // 停止空投锁
+    bool lock = false;
+    bool lockAirdrop = false;                    // 停止空投锁
 
-    uint256 public totalAirdropSupply;  // 空投数量
-    uint currentTotalAirdrop = 0;    	// 已经空投数量
-    uint airdrop = 1 ether;        		// 单个账户空投数量
-    // 存储是否空投过
-    mapping(address => bool) touched;
+    uint256 public totalAirdropSupply;          // 空投数量
+    uint public currentTotalAirdrop = 0;    	// 已经空投数量
+    uint public airdrop = 1 ether;        		// 单个账户空投数量
+    mapping(address => bool) public touched;    // 存储是否空投过
 
     /**
      * Constrctor function
      *
      * Initializes contract with initial supply tokens to the creator of the contract
      */
-    function NetkillerAirDropToken(
+    constructor(
         uint256 initialSupply,
         string tokenName,
-        string tokenSymbol
+        string tokenSymbol,
+        uint decimalUnits
     ) public {
         owner = msg.sender;
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
-        balanceOf[msg.sender] = totalSupply - totalAirdropSupply;  // 代币总量 - 空投总量 = 可支配代币量
         name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        symbol = tokenSymbol; 
+        decimals = decimalUnits;
+        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        balances[msg.sender] = totalSupply;                // Give the creator all initial token
     }
 
     modifier onlyOwner {
@@ -67,28 +66,33 @@ contract NetkillerAirDropToken {
     }
 
     modifier isLock {
-        require(!lock);
+        require(lock);
 	    _;
     }
     
-    function setLock(bool _lock) onlyOwner public {
+    function setLock(bool _lock) onlyOwner public{
         lock = _lock;
     }
 
     function transferOwnership(address newOwner) onlyOwner public {
-        owner = newOwner;
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
     }
- 
+    //function balanceOf(address _owner) public returns (uint256 balance) {
+    //    return balances[_owner];
+    //}
+    
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) isLock internal {
         require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
-        require (balanceOf[_from] >= _value);               // Check if the sender has enough
-        require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
+        require (balances[_from] >= _value);               // Check if the sender has enough
+        require (balances[_to] + _value > balances[_to]); // Check for overflows
         require(!frozenAccount[_from]);                     // Check if sender is frozen
         require(!frozenAccount[_to]);                       // Check if recipient is frozen
-        balanceOf[_from] -= _value;                         // Subtract from the sender
-        balanceOf[_to] += _value;                           // Add the same to the recipient
-        Transfer(_from, _to, _value);
+        balances[_from] -= _value;                         // Subtract from the sender
+        balances[_to] += _value;                           // Add the same to the recipient
+        emit Transfer(_from, _to, _value);
     }
 
     /**
@@ -113,8 +117,8 @@ contract NetkillerAirDropToken {
      * @param _value the amount to send
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-        require(_value <= allowance[_from][msg.sender]);     // Check allowance
-        allowance[_from][msg.sender] -= _value;
+        require(_value <= allowed[_from][msg.sender]);     // Check allowance
+        allowed[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
         return true;
     }
@@ -127,32 +131,14 @@ contract NetkillerAirDropToken {
      * @param _spender The address authorized to spend
      * @param _value the max amount they can spend
      */
-    function approve(address _spender, uint256 _value) public
-        returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    /**
-     * Set allowance for other address and notify
-     *
-     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
-     *
-     * @param _spender The address authorized to spend
-     * @param _value the max amount they can spend
-     * @param _extraData some extra information to send to the approved contract
-     */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData)
-        public
-        returns (bool success) {
-        tokenRecipient spender = tokenRecipient(_spender);
-        if (approve(_spender, _value)) {
-            spender.receiveApproval(msg.sender, _value, this, _extraData);
-            return true;
-        }
+    function allowance(address _owner, address _spender) view public returns (uint256 remaining) {
+      return allowed[_owner][_spender];
     }
-
     /**
      * Destroy tokens
      *
@@ -161,10 +147,10 @@ contract NetkillerAirDropToken {
      * @param _value the amount of money to burn
      */
     function burn(uint256 _value) onlyOwner public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
-        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        require(balances[msg.sender] >= _value);   // Check if the sender has enough
+        balances[msg.sender] -= _value;            // Subtract from the sender
         totalSupply -= _value;                      // Updates totalSupply
-        Burn(msg.sender, _value);
+        emit Burn(msg.sender, _value);
         return true;
     }
 
@@ -177,12 +163,12 @@ contract NetkillerAirDropToken {
      * @param _value the amount of money to burn
      */
     function burnFrom(address _from, uint256 _value) onlyOwner public returns (bool success) {
-        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
-        require(_value <= allowance[_from][msg.sender]);    // Check allowance
-        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
-        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        require(balances[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowed[_from][msg.sender]);    // Check allowance
+        balances[_from] -= _value;                         // Subtract from the targeted balance
+        allowed[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
         totalSupply -= _value;                              // Update totalSupply
-        Burn(_from, _value);
+        emit Burn(_from, _value);
         return true;
     }
 
@@ -190,10 +176,10 @@ contract NetkillerAirDropToken {
     /// @param target Address to receive the tokens
     /// @param mintedAmount the amount of tokens it will receive
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
-        balanceOf[target] += mintedAmount;
+        balances[target] += mintedAmount;
         totalSupply += mintedAmount;
-        Transfer(0, this, mintedAmount);
-        Transfer(this, target, mintedAmount);
+        emit Transfer(0, this, mintedAmount);
+        emit Transfer(this, target, mintedAmount);
     }
 
     /// @notice `freeze? Prevent | Allow` `target` from sending & receiving tokens
@@ -201,55 +187,26 @@ contract NetkillerAirDropToken {
     /// @param freeze either to freeze it or not
     function freezeAccount(address target, bool freeze) onlyOwner public {
         frozenAccount[target] = freeze;
-        FrozenFunds(target, freeze);
+        emit FrozenFunds(target, freeze);
     }
-    
-    function transfer(address _to, uint256 _value, bytes _data) public returns (bool) {
-        require(_to != address(this));
-        transfer(_to, _value);
-        require(_to.call(_data));
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value, bytes _data) public returns (bool) {
-        require(_to != address(this));
-
-        transferFrom(_from, _to, _value);
-
-        require(_to.call(_data));
-        return true;
-    }
-
-    function approve(address _spender, uint256 _value, bytes _data) public returns (bool) {
-        require(_spender != address(this));
-
-        approve(_spender, _value);
-
-        require(_spender.call(_data));
-
-        return true;
-    }
-
-    // 增发空投代币
-    function mintToken(uint256 _mintedAmount) onlyOwner public {
-        totalSupply += _mintedAmount;
-        totalAirdropSupply += _mintedAmount;
-    }
-
-    // 增发空投代币
+    // mint airdrop 
     function mintAirdropToken(uint256 _mintedAmount) onlyOwner public {
-        totalSupply += _mintedAmount;
+        require(balances[msg.sender] >= _mintedAmount);
+        totalSupply -= _mintedAmount;
         totalAirdropSupply += _mintedAmount;
     }
 
-    // 查询余额过程实现空投
+    function setAirdropLock(bool _lock) onlyOwner public{
+        lockAirdrop = _lock;
+    }
+    // airdrop coin
     function balanceOf(address _owner) public payable returns (uint256 balance) {
-        require(!lockAirdrop);
+        require(lockAirdrop);
         if (!touched[_owner] && currentTotalAirdrop < totalAirdropSupply) {
             touched[_owner] = true;
             currentTotalAirdrop += airdrop;
-            balanceOf[_owner] += airdrop;
+            balances[_owner] += airdrop;
         }
-        return balanceOf[_owner];
-    }    
+        return balances[_owner];
+    }
 }
