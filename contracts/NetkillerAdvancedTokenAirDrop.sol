c4239cfb7d3cf469886f777c9f08ab7208a8211f
Neo Chan <netkiller@msn.com>
2018-05-31 09:24:09 +0800

    Update NetkillerAdvancedTokenAirDrop.sol

--- contracts/NetkillerAdvancedTokenAirDrop.sol
+++ contracts/NetkillerAdvancedTokenAirDrop.sol
@@ -1,11 +1,11 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.24;
 
 /******************************************/
-/*       Netkiller ADVANCED TOKEN         */
+/*  Netkiller ADVANCED TOKEN - AirDrop    */
 /******************************************/
 /* Author netkiller <netkiller@msn.com>   */
 /* Home http://www.netkiller.cn           */
-/* Version 2018-05-16 - Add Global lock   */
+/* Version 2018-05-31 - Fixed transfer    */
 /******************************************/
 
 contract NetkillerAdvancedTokenAirDrop {
@@ -108,8 +108,9 @@ contract NetkillerAdvancedTokenAirDrop {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success){
         _transfer(msg.sender, _to, _value);
+        return true;
     }
 
     /**
