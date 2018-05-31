e50e8938633d0c9d05f9ac48e3d7e8e2fa5910c5
Neo Chan <netkiller@msn.com>
2018-05-31 09:19:51 +0800

    Update NetkillerMiniToken.sol

--- contracts/NetkillerMiniToken.sol
+++ contracts/NetkillerMiniToken.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.4.24;
 
 /******************************************/
-/*       Netkiller ADVANCED TOKEN         */
+/*       Netkiller Mini TOKEN             */
 /******************************************/
 /* Author netkiller <netkiller@msn.com>   */
 /* Home http://www.netkiller.cn           */
-/* Version 2018-05-23                     */
+/* Version 2018-05-31 Fixed transfer bool */
 /******************************************/
 
 contract NetkillerMiniToken {
@@ -73,8 +73,9 @@ contract NetkillerMiniToken {
      * @param _to The address of the recipient
      * @param _value the amount to send
      */
-    function transfer(address _to, uint256 _value) public {
+    function transfer(address _to, uint256 _value) public returns (bool success){
         _transfer(msg.sender, _to, _value);
+        return true;
     }
 
     /**
