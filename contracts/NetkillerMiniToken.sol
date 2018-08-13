d2cfd4f72455d42c829aeab6937d7470f35d151f
Neo Chan <netkiller@msn.com>
2018-08-13 13:02:31 +0800

    Update NetkillerMiniToken.sol

--- contracts/NetkillerMiniToken.sol
+++ contracts/NetkillerMiniToken.sol
@@ -57,7 +57,7 @@ contract NetkillerMiniToken {
  
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) internal {
-        require (_to != 0x0);                               // Prevent transfer to 0x0 address. Use burn() instead
+        require (_to != address(0));                               // Prevent transfer to 0x0 address. Use burn() instead
         require (balanceOf[_from] >= _value);               // Check if the sender has enough
         require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
         balanceOf[_from] -= _value;                         // Subtract from the sender
